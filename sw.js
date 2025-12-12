570c26596ca919ddbacd89b1052317a80aa2e98f
Adilson C. Rafael <adilsonrafael847@gmail.com>
2025-12-12 19:01:54 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+// sw.js
+const CACHE_NAME = 'chorty-v2.2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  // Adicione outros recursos se necessário
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
